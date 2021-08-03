d81a6f3cbde3dc295902d559bb6464baddbec5c2
Adam <aearanha@hotmail.com>
2021-08-03 11:03:09 -0400

    changed app.get to include path.join

--- app/server.js
+++ app/server.js
@@ -9,13 +9,13 @@ app.use(express.static('../tempClient')) // Works locally, untested on heroku
 
 // app.use(express.static(path.join(__dirname, '../tempClient'))); // Works locally, untested on heroku
 
-app.get('*', (req, res) => {
-    res.sendFile('tempClient/index.html');
-})
-
 // app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// })
+//     res.sendFile('tempClient/index.html');
+// })  // Did not work in heroku - Internal Server Error
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../tempClient', 'index.html'));
+})
 
 
 app.listen(PORT, () => {
