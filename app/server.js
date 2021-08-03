499925f909f1e139e49db203807aafb58af990df
Adam <aearanha@hotmail.com>
2021-08-03 10:54:05 -0400

    Testing basix express.static statement on heroku

--- app/server.js
+++ app/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+
+app.use(express.static('../tempClient')) // Works locally, untested on heroku
+
+// app.use(express.static(path.join(__dirname, '../tempClient'))); // Works locally, untested on heroku
 
 app.listen(PORT, () => {
     console.log(`Listening on port http://localhost:${PORT}`)
