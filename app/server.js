4707f1d48097aa3327a312bee2bf2eb46ea00fb5
Adam <aearanha@hotmail.com>
2021-08-03 11:46:29 -0400

    Added react

--- app/server.js
+++ app/server.js
@@ -4,19 +4,26 @@ const PORT = process.env.PORT || 5000
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, '../tempClient/build')));
+// app.use(express.static(path.join(__dirname, '../tempClient/build'))); // This works
 
-// app.use(express.static('../tempClient')) // Works locally, untested on heroku
+// // app.use(express.static('../tempClient')) // Works locally, untested on heroku
 
-// app.use(express.static(path.join(__dirname, '../tempClient'))); // Works locally, untested on heroku
+// // app.use(express.static(path.join(__dirname, '../tempClient'))); // Works locally, untested on heroku
+
+// // app.get('*', (req, res) => {
+// //     res.sendFile('tempClient/index.html');
+// // })  // Did not work in heroku - Internal Server Error
 
 // app.get('*', (req, res) => {
-//     res.sendFile('tempClient/index.html');
-// })  // Did not work in heroku - Internal Server Error
+//     res.sendFile(path.join(__dirname, '../tempClient/build', 'index.html'));
+// }) // This works in heroku
+
+app.use(express.static(path.join(__dirname, '../client/build'))); // This works
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../tempClient/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 }) // This works in heroku
+
 
 
 app.listen(PORT, () => {
